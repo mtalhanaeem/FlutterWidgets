c84113b03c18bd5d076d7c11f287aaad6666c076
M Talha Naeem <talha@stepbytech.com>
2025-05-09 18:04:04 +0400

    Second Commit

---
+++ lib/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary colors
+  static const Color primary = Color.fromRGBO(189, 150, 91, 1);
+  static const Color primaryVariant = Color(0xFF3700B3);
+  static const Color secondary = Color.fromRGBO(33, 35, 37, 1);
+  static const Color secondaryVariant = Color(0xFF018786);
+
+  // Background colors
+  static const Color screenBackground = Color.fromRGBO(223, 213, 202, 1);
+  static const Color surface = Color(0xFFFFFFFF);
+
+  // Error color
+  static const Color error = Color(0xFFB00020);
+
+  // Text colors
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onSecondary = Color(0xFF000000);
+  static const Color onBackground = Color(0xFF000000);
+  static const Color onSurface = Color(0xFF000000);
+  static const Color onError = Color(0xFFFFFFFF);
+}
